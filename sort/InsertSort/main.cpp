7b56438b0f9bd38daee964fce39aa094d69798bb
陆泽辉luzh <zehui.lzh@alibaba-inc.com>
2019-05-23 11:04:33 +0800

    Add the insert sort.

---
+++ sort/InsertSort/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+template <class T>
+int getArrayLength(T& array) {
+    return sizeof(array) / sizeof(array[0]);
+}
+
+template <class T>
+void insertSort(T* arr, int len) {
+    for (int i = 1; i < len; i++) {
+        for (int j = i; j > 0; j--) {
+            if (arr[j] < arr[j-1]) {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int arr[] = {1, 4, 3, 5, 2, 9, 6, 7};
+    int len = getArrayLength(arr);
+    insertSort(arr, len);
+    for (int i = 0; i < len; i++) {
+        std::cout << arr[i];
+        std::cout << ",";
+    }
+    return 0;
+}
